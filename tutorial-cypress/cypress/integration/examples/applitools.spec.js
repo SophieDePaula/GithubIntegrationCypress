9bb97a3e3cd0cd8a10c9e6fb5ed8444e1e5d8bdc
Sophie De Paula <sophie24587@gamil.com>
2020-10-21 15:23:08 -0700

    test1

--- tutorial-cypress/cypress/integration/examples/applitools.spec.js
+++ tutorial-cypress/cypress/integration/examples/applitools.spec.js
@@ -3,8 +3,7 @@ describe('Hello Cypress, This is Applitools', () => {
     cy.visit('https://applitools.com/');
     cy.eyesOpen({
       appName: 'Applitools1',
-      testName: 'Github Cypress Integration',
-      browser: { width: 800, height: 600 },
+      testName: 'Github Cypress Integration'
     });
     cy.eyesCheckWindow('Login Page');
     cy.eyesClose();
