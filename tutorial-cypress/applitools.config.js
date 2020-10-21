7fe386d160b9d575797532b9b6a24b6c2c8ab70a
Sophie De Paula <sophie24587@gamil.com>
2020-10-21 15:14:39 -0700

    test1

--- tutorial-cypress/applitools.config.js
+++ tutorial-cypress/applitools.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-    dontCloseBatches: true,
-    notifyOnCompletion: true
-  }
+  saveDebugData: false,
+  dontCloseBatches: true,
+  disableBrowserFetching: true,
+  concurrency: 25,
+  useDom: true,
+  enablePatterns: true,
+  failCypressOnDiff: false,
+  browser: [
+    { width: 1280, height: 720 },
+    { width: 375, height: 720 }
+  ]
+};
